934b233754856fb8932b7bd78a0587f81a0b5b9a
thirandev <thiranhettiarachchi@gmail.com>
2022-10-19 20:53:39 +0530

    Initial Commit and Project Setup

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb-typescript',
+        'prettier',
+        'plugin:import/typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json', // could be tsconfig.json too
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    rules: {
+        'no-console': 2,
+        'prettier/prettier': 2,
+        'react/function-component-definition': [
+            2,
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
+        'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+            },
+            extends: ['plugin:@typescript-eslint/recommended'],
+
+            parserOptions: {
+                project: ['./tsconfig.json'], // Specify it only for TypeScript files
+            },
+        },
+    ],
+};
