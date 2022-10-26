6d7fd044913eec45409b082ade14172a6e52a9fc
thirandev <thiranhettiarachchi@gmail.com>
2022-10-26 09:36:21 +0530

    Completed Icon atom and unit test

---
+++ src/components/atoms/LAIcon/index.tsx
@@ -0,0 +1,103 @@
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import { ViewStyle } from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import theme from '../../../utils/theme';
+import { AtLeast } from '../../../utils/types';
+import { styles } from './styles';
+
+const { scale, colors } = theme;
+
+export enum IconSize {
+    xSmall = scale.sc12,
+    small = scale.sc16,
+    medium = scale.sc24,
+    large = scale.sc32,
+    xLarge = scale.sc40,
+    xxLarge = scale.sc48,
+}
+
+interface Props {
+    name: string;
+    size: IconSize;
+    color: string;
+    style: ViewStyle;
+    onPress: () => void;
+    enableBackground: boolean;
+    backgroundColor: string;
+    touchableRef: React.LegacyRef<MaterialIcon>;
+    increasePadding: number;
+    testID: string;
+}
+
+const LAIcon = ({
+    size = IconSize.large,
+    name,
+    color,
+    style,
+    backgroundColor = colors.secondaryGray,
+    touchableRef,
+    increasePadding = 1,
+    enableBackground,
+    onPress,
+    testID,
+}: AtLeast<Props, 'name'>) => {
+    let iconSize = size;
+    let padding = 0;
+
+    if (enableBackground) {
+        switch (size) {
+            case IconSize.xSmall:
+                iconSize = scale.sc8;
+                padding = increasePadding * scale.sc2;
+                break;
+            case IconSize.small:
+                iconSize = scale.sc12;
+                padding = increasePadding * scale.sc2;
+                break;
+            case IconSize.medium:
+                iconSize = scale.sc20;
+                padding = increasePadding * scale.sc2;
+                break;
+            case IconSize.large:
+                iconSize = scale.sc24;
+                padding = increasePadding * scale.sc4;
+                break;
+            case IconSize.xLarge:
+                iconSize = scale.sc32;
+                padding = increasePadding * scale.sc4;
+                break;
+            case IconSize.xxLarge:
+                iconSize = scale.sc40;
+                padding = increasePadding * scale.sc4;
+                break;
+            default:
+                break;
+        }
+    }
+
+    const styleObject = enableBackground
+        ? {
+              ...styles.iconWithBackground,
+              padding,
+              borderRadius: padding + iconSize / 2,
+              width: iconSize + 2 * padding,
+              backgroundColor,
+              ...style,
+          }
+        : { ...style };
+
+    return (
+        <MaterialIcon
+            name={name}
+            size={iconSize}
+            color={color}
+            onPress={onPress}
+            ref={touchableRef}
+            style={[styleObject]}
+            testID={testID}
+        />
+    );
+};
+
+export default LAIcon;
---
+++ src/utils/helpers/scalingUtil.ts
@@ -0,0 +1,15 @@
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+// Guideline sizes are based on mobile prototype
+const guidelineBaseWidth = 428;
+const guidelineBaseHeight = 926;
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size: number, factor = 0.5) =>
+    size + (scale(size) - size) * factor;
+const moderateVerticalScale = (size: number, factor = 0.5) =>
+    size + (verticalScale(size) - size) * factor;
+
+export { scale, verticalScale, moderateScale, moderateVerticalScale };
