0eff214cfb8553d35d3c182520a39df437e0d273
Kalana Ramesh <kalanaramesh16@gmail.com>
2022-10-27 13:21:18 +0530

    Navigatios seperated

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,50 +1,11 @@
-/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { AuthScreensParamList, RootScreensParamsList } from './types';
+import RootNavigator from './RootNavigator';
 
-import Drawer from '../components/organisms/Drawer';
-/* Screens */
-import Splash from '../screens/Splash';
-import Login from '../screens/Login';
-import EmployeeHome from '../screens/EmployeeHome';
-import ManagerHome from '../screens/ManagerHome';
-
-const DrawerNav = createDrawerNavigator<AuthScreensParamList>();
-const StackNav = createNativeStackNavigator<RootScreensParamsList>();
-
-/* Auth navigator contains all the screens after the authtication */
-const AuthNavigator = () => {
-    const MemoizedDrawer = React.useCallback(() => <Drawer />, []);
-
-    return (
-        <DrawerNav.Navigator
-            drawerContent={() => <MemoizedDrawer />}
-            screenOptions={{
-                headerShown: false,
-                drawerType: 'front',
-                headerTitle: '',
-            }}>
-            <DrawerNav.Screen name='EmployeeHome' component={EmployeeHome} />
-            <DrawerNav.Screen name='ManagerHome' component={ManagerHome} />
-        </DrawerNav.Navigator>
-    );
-};
-
-/* Root navigator contains the screens before authentication */
-const RootNavigator = () => (
+const Navigators = () => (
     <NavigationContainer>
-        <StackNav.Navigator
-            screenOptions={{
-                headerShown: false,
-            }}>
-            <StackNav.Screen name='Splash' component={Splash} />
-            <StackNav.Screen name='Login' component={Login} />
-            <StackNav.Screen name='Auth' component={AuthNavigator} />
-        </StackNav.Navigator>
+        <RootNavigator />
     </NavigationContainer>
 );
 
-export default RootNavigator;
+export default Navigators;
