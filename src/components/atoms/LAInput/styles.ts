89869b0791e115cd23b05b68f832b6e8e66538d0
Kalana Ramesh <kalanaramesh16@gmail.com>
2022-10-31 12:54:03 +0530

    styles refactored

--- src/components/atoms/LAInput/styles.ts
+++ src/components/atoms/LAInput/styles.ts
@@ -14,40 +14,60 @@ interface Props {
 
 const { colors, fontSize, scale } = theme;
 
-export const styles = ({ type, disabled, focused, value, error }: Props) =>
-    StyleSheet.create({
+export const styles = ({ type, disabled, focused, value, error }: Props) => {
+    const labelFontSize =
+        type === 'LARGE'
+            ? fontSize.fs18
+            : type === 'SMALL'
+            ? fontSize.fs14
+            : fontSize.fs16;
+
+    const labelColor = disabled
+        ? colors.disabledColor
+        : error
+        ? colors.error
+        : colors.black;
+
+    const inputContainerBoarderColor = disabled
+        ? colors.disabledColor
+        : error
+        ? colors.error
+        : colors.black;
+
+    const inputContainerBackgroundColor = disabled
+        ? 'transparent'
+        : error
+        ? colors.errorBackground
+        : focused || value
+        ? colors.white
+        : colors.tertiaryColor;
+
+    const inputColor = disabled
+        ? colors.disabledColor
+        : error
+        ? colors.error
+        : colors.black;
+
+    const captionColor = disabled
+        ? colors.disabledColor
+        : error
+        ? colors.error
+        : colors.primaryGray;
+
+    return StyleSheet.create({
         container: {
             margin: scale.sc8,
         },
         label: {
             fontWeight: type === 'COMMENT' ? '400' : '500',
-            fontSize:
-                type === 'LARGE'
-                    ? fontSize.fs18
-                    : type === 'SMALL'
-                    ? fontSize.fs14
-                    : fontSize.fs16,
-            color: disabled
-                ? colors.disabledColor
-                : error
-                ? colors.error
-                : colors.black,
+            fontSize: labelFontSize,
+            color: labelColor,
         },
         inputContainer: {
             flexDirection: 'row',
             borderWidth: disabled || error || focused || value ? scale.vsc1 : 0,
-            borderColor: disabled
-                ? colors.disabledColor
-                : error
-                ? colors.error
-                : colors.black,
-            backgroundColor: disabled
-                ? 'transparent'
-                : error
-                ? colors.errorBackground
-                : focused || value
-                ? colors.white
-                : colors.tertiaryColor,
+            borderColor: inputContainerBoarderColor,
+            backgroundColor: inputContainerBackgroundColor,
             borderRadius: theme.radius.rd8,
             paddingVertical: type === 'SMALL' ? scale.vsc10 : scale.vsc16,
             paddingHorizontal: scale.sc16,
@@ -57,24 +77,17 @@ export const styles = ({ type, disabled, focused, value, error }: Props) =>
         input: {
             flex: 1,
             marginHorizontal: scale.sc6,
-            color: disabled
-                ? colors.disabledColor
-                : error
-                ? colors.error
-                : colors.black,
+            color: inputColor,
         },
         commentInput: {
             minHeight: scale.vsc110,
         },
         caption: {
             fontSize: fontSize.fs12,
-            color: disabled
-                ? colors.disabledColor
-                : error
-                ? colors.error
-                : colors.primaryGray,
+            color: captionColor,
             marginTop: scale.vsc8,
         },
     });
+};
 
 export default styles;
