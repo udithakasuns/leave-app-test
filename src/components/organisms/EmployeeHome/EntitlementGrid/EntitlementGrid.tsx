ef9aae41ddf6211dd5d08d57b0a6ddb5a2262456
thirandev <thiranhettiarachchi@gmail.com>
2022-11-05 21:21:18 +0530

    Entitlement Grid

---
+++ src/components/organisms/EmployeeHome/EntitlementGrid/EntitlementGrid.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FlatList, Pressable, ScrollView } from 'react-native';
+import { LeaveCard } from 'src/components/atoms';
+import { getLeaveUnicode } from 'src/utils/helpers/unicodeHandler';
+import { Entitlement, TestProps } from 'src/utils/types';
+
+export type EntitlementSelection = Entitlement & {
+    isSelected?: boolean;
+};
+
+interface Props extends Partial<TestProps> {
+    entitlements: EntitlementSelection[];
+    onEntitlementPress: (item: Entitlement) => void;
+}
+
+const EntitlementGrid = ({ entitlements, onEntitlementPress }: Props) => {
+    const renderItem = ({ item }: { item: Entitlement }) => (
+        <Pressable onPress={() => onEntitlementPress(item)}>
+            <LeaveCard
+                takenLeaves={item.totalDaysUsed.toString()}
+                totalLeaves={item.balanceInDays.toString()}
+                uniCodeIcon={getLeaveUnicode(item.leaveType)}
+                leaveType={item.leaveType.name}
+            />
+        </Pressable>
+    );
+    return (
+        <ScrollView
+            horizontal
+            contentContainerStyle={{ width: '100%', height: '100%' }}>
+            <FlatList
+                data={entitlements}
+                renderItem={renderItem}
+                keyExtractor={item => item.entitlementId.toString()}
+                style={{ flexGrow: 0 }}
+                numColumns={3}
+                columnWrapperStyle={{
+                    overflow: 'visible',
+                }}
+            />
+        </ScrollView>
+    );
+};
+
+export default EntitlementGrid;
