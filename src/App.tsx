309f79bcb7b0ddefa547236f62420873244894d8
kalana-rootcode <kalana.ramesh@rootcodelabs.com>
2022-11-21 11:14:00 +0530

    IOS Splash screen configuration completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import RootNavigator from './navigators';
 
-const App: React.FC = () => <RootNavigator />;
+const App: React.FC = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
+    return <RootNavigator />;
+};
 
 export default App;
