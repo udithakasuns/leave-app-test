3973265ac3858b626da0c4d4a094eb5f4bee17d4
thirandev <thiranhettiarachchi@gmail.com>
2022-11-30 22:59:52 +0530

    More Details Button

---
+++ src/components/molecules/LAMoreDetailsButton/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
+import { View } from 'react-native';
+import { Divider } from 'src/components/atoms';
+import theme from 'src/utils/theme';
+import { SelectionButton } from '..';
+
+export type Props = {
+    onPress: () => void;
+};
+
+const LAMoreDetailsButton = ({ onPress }: Props) => (
+    <View>
+        <Divider />
+        <SelectionButton
+            buttonStyle={{ backgroundColor: theme.colors.white }}
+            label='View More Details'
+            onPress={onPress}
+        />
+        <Divider />
+    </View>
+);
+
+export default LAMoreDetailsButton;
