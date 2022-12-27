381dd93568920757b1b8a838b350ccd8636d93de
kalana-rootcode <kalana.ramesh@rootcodelabs.com>
2022-12-27 18:46:15 +0530

    Common back handler hook called for all screenes

--- src/screens/ManagerViewAll/index.tsx
+++ src/screens/ManagerViewAll/index.tsx
@@ -1,15 +1,12 @@
-import { useNavigation } from '@react-navigation/native';
 import { UseInfiniteQueryResult } from '@tanstack/react-query';
-import {
-    DrawerScreenNavigationProp,
-    ManagerViewAllScreensProps,
-} from 'navigators/types';
+import { ManagerViewAllScreensProps } from 'navigators/types';
 import React from 'react';
 import { ScrollView, View } from 'react-native';
 import { Spacer, Text } from 'src/components/atoms';
 import { BackHeader } from 'src/components/molecules';
 import LAPendingRequestList from 'src/components/organisms/ManagerHome/LAPendingRequestList';
 import { useManagerFilterStore, useManagerStore } from 'src/store';
+import useBackAction from 'src/utils/hooks/useBackAction';
 import { useAllPendingRequestData } from 'src/utils/hooks/usePendingRequestData';
 import theme from 'src/utils/theme';
 import { Page, PendingRequestType } from 'src/utils/types';
@@ -18,9 +15,10 @@ import { screenStyles } from 'utils/styles';
 const { scale } = theme;
 
 const ManagerViewAll: React.FC<ManagerViewAllScreensProps> = () => {
-    const navigation = useNavigation<DrawerScreenNavigationProp>();
     const { getManagerModal } = useManagerStore();
     const { params, resetFiltersParams } = useManagerFilterStore();
+
+    const backAction = useBackAction();
 
     const {
         data: leaveRequests,
@@ -35,9 +33,9 @@ const ManagerViewAll: React.FC<ManagerViewAllScreensProps> = () => {
         getManagerModal(item.leaveRequestId);
     };
 
-    const backAction = () => {
+    const onGoBack = () => {
         resetFiltersParams();
-        navigation.jumpTo('ManagerHome');
+        backAction();
         return true;
     };
 
@@ -49,7 +47,7 @@ const ManagerViewAll: React.FC<ManagerViewAllScreensProps> = () => {
 
     return (
         <View style={screenStyles.container}>
-            <BackHeader title='Home' onBackPress={backAction} />
+            <BackHeader title='Home' onBackPress={onGoBack} />
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Spacer />
                 <Text type='H1Bold' style={{ marginHorizontal: scale.sc5 }}>
