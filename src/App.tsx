39c3a84666d64dc76530ef4864404164069eebe1
kalana-rootcode <kalana.ramesh@rootcodelabs.com>
2023-01-30 23:02:07 +0530

    Code push completed 90%

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import codePush from 'react-native-code-push';
 import Navigators from './navigators';
 
 const App: React.FC = () => {
@@ -10,4 +11,8 @@ const App: React.FC = () => {
     return <Navigators />;
 };
 
-export default App;
+const codePushOptions = {
+    checkFrequency: codePush.CheckFrequency.ON_APP_START,
+};
+
+export default codePush(codePushOptions)(App);
