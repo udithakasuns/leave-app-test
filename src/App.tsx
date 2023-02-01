5e49f780de72ded1973203b95270527a715ca97e
kalana-rootcode <kalana.ramesh@rootcodelabs.com>
2023-02-01 18:50:00 +0530

    CodePush integration completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import codePush from 'react-native-code-push';
 import Navigators from './navigators';
+import {
+    CODEPUSH_DEPLOYMENT_KEY_ANDROID,
+    CODEPUSH_DEPLOYMENT_KEY_IOS,
+} from './configs';
 
 const App: React.FC = () => {
     useEffect(() => {
@@ -14,5 +19,14 @@ const App: React.FC = () => {
 const codePushOptions = {
     checkFrequency: codePush.CheckFrequency.ON_APP_START,
 };
+
+/* Based on the Key, new app updates will be reflacted in particular environments */
+codePush.sync({
+    installMode: codePush.InstallMode.IMMEDIATE,
+    deploymentKey:
+        Platform.OS === 'ios'
+            ? CODEPUSH_DEPLOYMENT_KEY_IOS
+            : CODEPUSH_DEPLOYMENT_KEY_ANDROID,
+});
 
 export default codePush(codePushOptions)(App);
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,9 +3,11 @@ export type DeploymentEnv = 'STG' | 'QA' | 'PROD';
 export type Config = {
     apiBaseUrl: string;
     webBaseUrl: string;
+    codePushDeploymentKeyAndroid: string /* If you update this key you need to update in android/app/build.gralde file as well */;
+    codePushDeploymentKeyIOS: string /* If you update this key you need to update in ios/rtc_leave_app/build_settings/CODEPUSH_KEY file as well */;
 };
 
-export const DEPLOYMENT_ENV: DeploymentEnv = 'QA'; // Please change only this value when you are going to deploy
+export const DEPLOYMENT_ENV: DeploymentEnv = 'STG'; // Please change only this value when you are going to deploy
 
 const getCofig = (env: DeploymentEnv): Config => {
     // staging config set as default
@@ -13,23 +15,38 @@ const getCofig = (env: DeploymentEnv): Config => {
         return {
             apiBaseUrl: 'https://api.rootcode.myleave.io',
             webBaseUrl: 'https://rootcode.myleave.io',
+            codePushDeploymentKeyAndroid:
+                'HLY-jQoQqNNzU8LR-a9IyLoa7zUeC_Mmx4Zq-',
+            codePushDeploymentKeyIOS: '6s6ITQbaSiHDri4iWsay7Yd5h0mxn9j9jG6-Z',
         };
     }
     if (env === 'QA') {
         return {
             apiBaseUrl: 'https://qa-api.myleave.rootcode.software',
             webBaseUrl: 'https://qa.myleave.rootcode.software',
+            codePushDeploymentKeyAndroid:
+                'BsPX5NQculfTeErzU7E2XmBEtwXBfmJrJ1Nta',
+            codePushDeploymentKeyIOS: 'r6u0Vp1YG_8XRbwQy-2binA1yEmo5aYypYNTl',
         };
     }
     // STG
     return {
         apiBaseUrl: 'https://stage-api.leaveapp.rootcode.software',
         webBaseUrl: 'https://leaveapplication.rootcode.software',
+        codePushDeploymentKeyAndroid: 'oXlAvLeGXzGHw4cn4F6sGEgd3UAOXSpu3rZc_',
+        codePushDeploymentKeyIOS: 'P6qN6-Dql-q_NiOGPO3Etz_nCOZ2bVW6sm3nn',
     };
 };
 
-const { apiBaseUrl, webBaseUrl } = getCofig(DEPLOYMENT_ENV);
+const {
+    apiBaseUrl,
+    webBaseUrl,
+    codePushDeploymentKeyAndroid,
+    codePushDeploymentKeyIOS,
+} = getCofig(DEPLOYMENT_ENV);
 
 export const API_BASE_URL = apiBaseUrl;
 export const SUPPORT_URL = `${webBaseUrl}/support/questions`;
 export const PRIVACY_POLICY_URL = `${webBaseUrl}/support/privacy-policy`;
+export const CODEPUSH_DEPLOYMENT_KEY_ANDROID = codePushDeploymentKeyAndroid;
+export const CODEPUSH_DEPLOYMENT_KEY_IOS = codePushDeploymentKeyIOS;
