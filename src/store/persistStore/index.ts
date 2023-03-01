425c001528e36db57b21ad5c4419eb95dc75a96a
kalana-rootcode <kalana.ramesh@rootcodelabs.com>
2023-03-01 19:44:46 +0530

    Login loading issue fixed && Code refactored && Unneccessary code blocks removed

--- src/store/persistStore/index.ts
+++ src/store/persistStore/index.ts
@@ -6,18 +6,8 @@ import { State, Actions, Persist } from './types';
 const persistStore = create<State & Actions>(
     (persist as Persist)(
         set => ({
-            isAutherized: false,
-            authType: '',
             isDeviceRegistered: false,
             deviceUniqueId: null,
-            setIsAutherized: isAutherized =>
-                set(() => ({
-                    isAutherized,
-                })),
-            setAuthType: authType =>
-                set(() => ({
-                    authType,
-                })),
             setDeviceUniqueId: deviceUniqueId =>
                 set(() => ({
                     deviceUniqueId,
--- src/store/persistStore/types.ts
+++ src/store/persistStore/types.ts
@@ -1,17 +1,11 @@
 import { StateCreator } from 'zustand';
 import { PersistOptions } from 'zustand/middleware';
 
-type AuthType = 'social' | 'general' | '';
-
 export type State = {
-    isAutherized: boolean; // For authentication
-    authType: AuthType; // For authentication
     deviceUniqueId: string | null; // For notifications
 };
 
 export type Actions = {
-    setIsAutherized: (isAutherized: boolean) => void; // For authentication
-    setAuthType: (authType: AuthType) => void; // For authentication
     setDeviceUniqueId: (deviceUniqueId: string | null) => void;
 };
 
